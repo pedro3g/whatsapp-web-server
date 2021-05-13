d126f1a04cc36d110136091528f536ea4a855d5b
krelod <pgomesrodrigues801@gmail.com>
2021-05-13 17:22:58 +0000

    Ambiente configurado; servidor configurado;

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const http = require('http')
+const { Server } = require('socket.io')
+
+const app = express()
+const server = http.createServer(app)
+const io = new Server(server)
+const port = process.env.PORT || 3001
+
+io.on('connection', socket => {
+  console.info('A user connected')
+})
+
+server.listen(port)
